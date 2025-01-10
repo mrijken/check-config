683a63a3f97dbfcc2266d91ba43115dd527b96e0
Marc Rijken <marc@rijken.org>
2025-01-10 09:16:32 +0000

    add placeholder in file_regex

--- src/checkers/mod.rs
+++ src/checkers/mod.rs
@@ -187,7 +187,7 @@ fn get_check_from_check_table(
         "file_absent" => Box::new(file_absent::FileAbsent::new(generic_check)),
         "file_present" => {
             let placeholder = check_table
-                .get("placeholder")
+                .get("__placeholder__")
                 .map(|v| v.as_str().expect("placeholder is a string").to_string())
                 .unwrap_or("".to_string());
 
@@ -206,6 +206,9 @@ fn get_check_from_check_table(
                     .as_str()
                     .expect("__regex__ is a string")
                     .to_string(),
+                check_table
+                    .get("__placeholder__")
+                    .map(|v| v.as_str().expect("placeholder is a string").to_string()),
             ))
         }
         "lines_absent" => {
