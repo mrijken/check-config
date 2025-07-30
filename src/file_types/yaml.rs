f9f652e136e301cb97224a6ad0d57eb1f87b6800
Marc Rijken <marc@rijken.org>
2025-07-30 19:54:15 +0000

    remove obsolete function

--- src/file_types/yaml.rs
+++ src/file_types/yaml.rs
@@ -20,28 +20,3 @@ impl FileType for Yaml {
         crate::mapping::yaml::from_string(contents)
     }
 }
-
-fn _convert_value_to_item(value: &toml::Value) -> Value {
-    match value {
-        toml::Value::String(v) => Value::String(v.clone()),
-        toml::Value::Integer(v) => Value::Number(serde_yaml::Number::from(*v)),
-        toml::Value::Float(v) => Value::Number(serde_yaml::Number::from(*v)),
-        toml::Value::Boolean(v) => Value::Bool(*v),
-        toml::Value::Datetime(v) => Value::String(v.to_string()),
-        toml::Value::Array(v) => {
-            let mut a = vec![];
-            for v_item in v {
-                a.push(_convert_value_to_item(v_item))
-            }
-            Value::Sequence(a)
-        }
-        toml::Value::Table(v) => {
-            let mut a: Mapping = Mapping::new();
-            for (k, v_item) in v {
-                a.insert(Value::String(k.clone()), _convert_value_to_item(v_item));
-            }
-
-            Value::Mapping(a)
-        }
-    }
-}
